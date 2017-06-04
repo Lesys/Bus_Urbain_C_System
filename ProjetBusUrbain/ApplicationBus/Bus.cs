f22447e88086b7cad13697b388743ee0d288a661
Floxaga <florian.victoriadt@gmail.com>
2017-06-04 17:03:22 +0200

    BUs

---
+++ ProjetBusUrbain/ApplicationBus/Bus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Bus
+{
+	public Bus()
+	{
+        private Random nbplaces;
+        private Mutex porteAvant;
+        private Mutex porteArriere;
+        private boolean demandeArret;
+        
+    public int getNbPlaces()
+        {
+            return nbPlaces;
+        }
+
+    public void incNbPlaces()
+        {
+            Interlocked.Increment(ref nbPlaces);
+        }
+
+    public void decNbPlaces()
+    {
+        Interlocked.Decrement(ref nbPlaces);
+    }
+    public Mutex getMutexPA()
+    {
+        return PorteAvant;
+    }
+    public Mutex getMutexPA()
+    {
+        return porteArriere;
+    }
+    public ControlerPassager()
+    {
+        if (lePassager.getBillet() == true)
+        {
+            sleep(500);
+        }
+        else
+        {
+            sleep(2000);
+            lePassager.nouveaubillet();
+        }
+
+        return 0;
+    }
+    public quitterArret()
+    {
+        //quitter
+    }
+}
+}
