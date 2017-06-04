582059a0c40c8592ca0152a873dabdf3961b2f46
Floxaga <florian.victoriadt@gmail.com>
2017-06-04 17:30:42 +0200

    3

--- ProjetBusUrbain/ApplicationBus/Bus.cs
+++ ProjetBusUrbain/ApplicationBus/Bus.cs
@@ -2,72 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
-namespace ApplicationBus
+
+public class Bus
 {
-	public class Bus
+	public Bus()
 	{
-		#region Propriétés
+        private Random nbplaces;
+        private Mutex porteAvant;
+        private Mutex porteArriere;
+        private boolean demandeArret;
+        
+    public int getNbPlaces()
+        {
+            return nbPlaces;
+        }
 
-		private int nbPlaces;
-		private Mutex porteAvant;
-		private Mutex porteArriere;
-		private bool demandeArret;
+    public void incNbPlaces()
+        {
+            Interlocked.Increment(ref nbPlaces);
+        }
 
-		#endregion
+    public void decNbPlaces()
+    {
+        Interlocked.Decrement(ref nbPlaces);
+    }
+    public Mutex getMutexPA()
+    {
+        return PorteAvant;
+    }
+    public Mutex getMutexPA()
+    {
+        return porteArriere;
+    }
+    public ControlerPassager()
+    {
+        if (lePassager.getBillet() == true)
+        {
+            sleep(500);
+        }
+        else
+        {
+            sleep(2000);
+            lePassager.nouveaubillet();
+        }
 
-		#region Constructeurs
-
-		public Bus()
-		{
-		}
-
-		#endregion
-
-		#region Méthodes
-
-		public int getNbPlaces()
-		{
-			return nbPlaces;
-		}
-
-		public void incNbPlaces()
-		{
-			Interlocked.Increment(ref nbPlaces);
-		}
-
-		public void decNbPlaces()
-		{
-			Interlocked.Decrement(ref nbPlaces);
-		}
-		public Mutex getMutexPorteAvant()
-		{
-			return porteAvant;
-		}
-		public Mutex getMutexPorteArriere()
-		{
-			return porteArriere;
-		}
-		/*public void ControlerPassager()
-		{
-			if (lePassager.getBillet() == true)
-			{
-				sleep(500);
-			}
-			else
-			{
-				sleep(2000);
-				lePassager.nouveaubillet();
-			}
-
-			return 0;
-		}
-		public void quitterArret()
-		{
-			//quitter
-		}*/
-
-		#endregion
-	}
+        return 0;
+    }
+    public quitterArret()
+    {
+        //quitter
+    }
+}
 }
